63fe4200b562ae6ff21c05a2639d1228403b32b3
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2020-11-17 21:47:06 -0800

    Remove remnants of old project structure

--- bot/db/schemas/skillSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const skillSchema = new mongoose.Schema({
-    jack_of_all_trades: { type: Boolean, default: false },
-    proficiency: { type: Boolean, default: false },
-    expertise: { type: Boolean, default: false },
-});
-
-module.exports = skillSchema;
--- startup.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-if [[ `hostname` == *.local ]]; then
-    ENV="DEV"
-else
-    ENV="PRD"
-fi
-screen -S diendee
-node ./server.js
